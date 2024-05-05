9cb59842da397ea569b42c468600249b6f6442dd
Ashish Kumar <ashishbhu221306@gmail.com>
2024-05-05 22:18:41 +0530

    fixed

--- index.js
+++ index.js
@@ -29,25 +29,14 @@ app.use("/upload", express.static("upload"));
 app.use(authRouter);
 app.use(orderRouter);
 app.use(portfolioRouter);
-// app.use(uploadRouter);
+app.use(uploadRouter);
 app.use(mailRouter);
 app.use(dashbaordRouter);
 // app.use(multerErrorHandler);
 const port = process.env.PORT || 4000;
 app.listen(port, () => {});
 app.get("/", (req, res) => {
-    res.send(`
-    <h1>DEV Backend</h1>
-   
-  <style>*{margin:0;padding:0;body{overflow:hidden}}
-  h1{text-align:center}
-  body{font-family:sans-serif}
-  @media(max-width:767px){
-    img{object-fit:cover;height:100%;object-position:center}
-  }
-  </style>
-  <img width="100%" src="https://images.unsplash.com/photo-1573164713988-8665fc963095?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
-  `);
+    res.send("API RUNNING");
 });
 app.get("/roles", (req, res) => {
     res.json(roles);
