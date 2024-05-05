dce167988a03739cf76c9f29402411cc9861c225
Ashish Kumar <ashishbhu221306@gmail.com>
2024-05-05 23:10:22 +0530

    fixed static file serve

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/upload', express.static(path.join(__dirname, 'upload')));
+app.use("/upload", express.static(path.join(__dirname, "upload")));
 
 require("./db");
 const authRouter = require("./routes/auth");
@@ -45,18 +45,7 @@ app.get("/favicon.ico", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-    res.send(`
-    <h1>DEV Backend</h1>
-   
-  <style>*{margin:0;padding:0;body{overflow:hidden}}
-  h1{text-align:center}
-  body{font-family:sans-serif}
-
-    img{object-fit:cover;height:100vh;object-position:center}
-
-  </style>
-  <img width="100%" src="https://images.unsplash.com/photo-1573164713988-8665fc963095?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
-  `);
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/roles", (req, res) => {
