31dfa702bc97e403af6599055880988e9087e7af
Ashish Kumar <ashishbhu221306@gmail.com>
2024-05-05 22:47:31 +0530

    fixed

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'upload')));
 
 require("./db");
 const authRouter = require("./routes/auth");
