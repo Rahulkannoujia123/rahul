ebfe909b597f58030e768589719b6dd8521ae243
Ashish Kumar <ashishbhu221306@gmail.com>
2024-05-05 23:01:48 +0530

    fixed

--- utils/upload.js
+++ utils/upload.js
@@ -1,8 +1,8 @@
 const multer = require("multer");
 const storage = multer.diskStorage({
-    destination:function (req, file, cb) {
-        cb(null, 'upload/');
-      },
+    destination: function (req, file, cb) {
+        cb(null, "upload/");
+    },
     filename: (req, file, cb) => {
         return cb(
             null,
