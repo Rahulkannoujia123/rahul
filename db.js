2b37f8bead451173eabf53ff32b11552073a921f
Ashish Kumar <ashishbhu221306@gmail.com>
2024-07-21 20:12:58 +0530

    d

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 mongoose
     .connect(
-        // process.env.MONGO_URL,
-        "mongodb://127.0.0.1:27017/mydb",
+        process.env.MONGO_URL,
+        // "mongodb://127.0.0.1:27017/mydb",
         {
             useNewUrlParser: true,
             useUnifiedTopology: true,
